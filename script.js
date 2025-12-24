0be574870772524799edf2df8415959fc479c3af
hassanahmed-013 <hassanahmed78613@gmail.com>
2025-12-24 22:08:09 +0500

    Done

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const startBtn = document.getElementById("startBtn");
+
+    startBtn.addEventListener("click", function () {
+        alert("Welcome! You clicked the Get Started button 🚀");
+    });
+});
